ce665bbee986d1491561f34ee0b5f4d7a94018d0
chnpmy <chnpmy@gmail.com>
2014-11-02 14:21:19 +0800

    first

---
+++ ATM/FormATM.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ATM
+{
+    partial class FormATM
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.pnlBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblWelcome.Location = new System.Drawing.Point(3, 10);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(510, 46);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "欢迎使用天地银行自动取款机！";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.lblPassword);
+            this.panel1.Controls.Add(this.txtAccount);
+            this.panel1.Controls.Add(this.lblAccount);
+            this.panel1.Location = new System.Drawing.Point(8, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(483, 111);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblAccount.Location = new System.Drawing.Point(4, 4);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(125, 46);
+            this.lblAccount.TabIndex = 0;
+            this.lblAccount.Text = "账号：";
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtAccount.Location = new System.Drawing.Point(136, 4);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(344, 47);
+            this.txtAccount.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblPassword.Location = new System.Drawing.Point(4, 61);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(125, 46);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "密码：";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPassword.Location = new System.Drawing.Point(135, 61);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(344, 47);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(412, 176);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "提交";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.Controls.Add(this.lblWelcome);
+            this.pnlBottom.Controls.Add(this.panel1);
+            this.pnlBottom.Controls.Add(this.btnOK);
+            this.pnlBottom.Location = new System.Drawing.Point(12, 12);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(524, 313);
+            this.pnlBottom.TabIndex = 3;
+            // 
+            // FormATM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 337);
+            this.Controls.Add(this.pnlBottom);
+            this.Name = "FormATM";
+            this.Text = "天地银行自动取款机";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlBottom.ResumeLayout(false);
+            this.pnlBottom.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Panel pnlBottom;
+    }
+}
+
---
+++ ATM/FormATM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM
+{
+    public partial class FormATM : Form
+    {
+        public FormATM()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            pnlBottom.Controls.Clear();
+            FormMoney f = new FormMoney{TopLevel = false, FormBorderStyle = FormBorderStyle.None};
+            pnlBottom.Controls.Add(f); 
+            f.Show();
+        }
+    }
+}
